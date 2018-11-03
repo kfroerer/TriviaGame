e14404250f17a5d9e3cfc56d92c5d97dabd4e46e
kjojones <43149867+kfroerer@users.noreply.github.com>
2018-11-03 10:22:23 -0500

    needs timer tweak, reset button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
-var score = 0;
-var timeLeft = 30;
-var intervalID = setInterval(countdown, 1000)
+var score= 0;
+var timeLeft = 20;
+var intervalID = setInterval(countdown, 1000);
+
 var questionBank = [
     {
         ask: "What color is the couch at Central Perk?",
@@ -55,47 +56,43 @@ var questionBank = [
     }
 ]
 
-// function timeConverter(t) {
+function timeConverter(t) {
 
-//     var minutes = Math.floor(t / 60);
-//     var seconds = t - (minutes * 60);
+    var minutes = Math.floor(t / 60);
+    var seconds = t - (minutes * 60);
 
-//     if (seconds < 10) {
-//     seconds = "0" + seconds;
-//     }
-
-//     if (minutes === 0) {
-//     minutes = "00";
-//     }
-//     else if (minutes < 10) {
-//     minutes = "0" + minutes;
-//     }
-
-//     return minutes + ":" + seconds;
-// }
-// var converted = timeConverter(timeLeft);
-// intervalID = setInterval(countdown, 1000)
-function countdown() {  
-        if (timeLeft == -1){
-        clearTimeout(intervalID);
-        $(".question-field").empty()
-        $("#instructions").text("Your final score: " + score + "/10!")
-    }else {
-        $("#timer").text(timeLeft + " seconds remaining")
-        timeLeft--;
+    if (seconds < 10) {
+    seconds = "0" + seconds;
     }
 
-};
+    if (minutes === 0) {
+    minutes = "00";
+    }
+    else if (minutes < 10) {
+    minutes = "0" + minutes;
+    }
+
+    return minutes + ":" + seconds;
+}
+var converted = timeConverter(timeLeft);
+
+
+
 
 var gameStart = function(){
-       
+    var timerDiv = $("<div>");
+    timerDiv.attr("id", "timer");
+    $("#instructions").after(timerDiv);
+    var resetDiv = $("<div>");
+    resetDiv.addClass("button resetButton");
+    
     for (var i = 0; i < questionBank.length; i++){
         var QuestionDiv = $("<div>");
         var questionLine = $("<p>");
         questionLine.addClass("questions");
         QuestionDiv.append(questionLine);
         questionLine.append(questionBank[i].ask);
-
+        
         for (var j = 0; j < questionBank[i].choices.length; j++){
             var button = $("<button>");
             button.addClass("button choices-button");
@@ -105,26 +102,43 @@ var gameStart = function(){
             button.text(buttonText);
             QuestionDiv.append(button);
             $(".question-field").append(QuestionDiv);
-            }
+        }
     }
     $("#instructions").remove();
     
 }
 
 $("#start-button").on("click", function(){
-   gameStart();
-  
-    });
+    gameStart();
+    
+});
+function countdown() {  
+    if (timeLeft == 0){
+        clearTimeout(intervalID);
+        $(".question-field").empty();
+        $("#timer").text("Your final score: " + score + "/10!");
+        var resetButton = $("<button>");
+        resetButton.addClass("button resetButton");
+        resetButton.text("Play Again")
+        $("main").append(resetButton)
+    }else {
+        timeLeft--;
+        $("#timer").text(timeLeft + " remaining")
+    }
+
+};
+
+
 
 $(document).on("click", ".choices-button", function(){
     console.log("button clicked");
+   
     var userInput = $(this).attr("id");
     
     var index = $(this).attr("index");
     // console.log(score);
     $(this).addClass("clicked");
-    
-    //disable other sibling elements - jquery 
+    $(this).siblings().prop("disabled", true);    
 
     if (userInput === questionBank[index].correct){
         score++;
@@ -134,6 +148,7 @@ $(document).on("click", ".choices-button", function(){
 });
  
 
+//do a reset function at with a timeout or a button to reset and attach it to reset button
 
 
 
